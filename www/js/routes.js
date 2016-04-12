929a68bb8319a0e8b58fbf3b8dc6bbbc718e677e
Vergne Victor <vergne.victor@gmail.com>
2016-04-12 15:15:56 +0200

    addings professional routes

--- www/js/routes.js
+++ www/js/routes.js
@@ -34,8 +34,7 @@ angular.module('app.routes', [])
     url: '/page3',
     views: {
       'side-menu21': {
-        templateUrl: 'templates/cloud.html',
-        controller: 'cloudCtrl'
+        templateUrl: 'templates/patient.html',
       }
     }
   })
@@ -51,6 +50,51 @@ angular.module('app.routes', [])
     views: {
       'side-menu21': {
         templateUrl: 'templates/medecin.html',
+      }
+    }
+  })
+
+  .state('menu.cardiologue', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cardiologue.html',
+      }
+    }
+  })
+
+  .state('menu.dentiste', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/dentiste.html',
+      }
+    }
+  })
+
+  .state('menu.infirmier', {
+    url: '/page7',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/infirmier.html',
+      }
+    }
+  })
+
+  .state('menu.kinesitherapeute', {
+    url: '/page8',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/kinesitherapeute.html',
+      }
+    }
+  })
+
+  .state('menu.liberal', {
+    url: '/page9',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/liberal.html',
       }
     }
   })
