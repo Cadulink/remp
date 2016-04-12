64ffb5b4ad2e564973b623f87965e4da2dccc5ca
Vergne Victor <vergne.victor@gmail.com>
2016-04-12 11:25:41 +0200

    initial

---
+++ www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('remplacementCtrl', function($scope) {
+
+})
+   
+.controller('cartCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+      
+.controller('mDecinCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+ 
